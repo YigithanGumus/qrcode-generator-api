51f14d77738d9ecd404d3ea8c7dc4ce05a1ac63c
YigithanGumus <yigithangumuss@gmail.com>
2024-11-19 02:09:26 +0300

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,16 +1,23 @@
-const { Sequelize } = require('sequelize');
+const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize('yigitha4_sequelize', 'yigitha4_yigithan', '2kp4kxE3iufw3HF', {
-  host: '104.247.165.67',
-  dialect: 'mysql', /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */
-});
+const sequelize = new Sequelize(
+  "yigitha4_sequelize",
+  "yigitha4_yigithan",
+  "2kp4kxE3iufw3HF",
+  {
+    host: "104.247.165.67",
+    dialect:
+      "mysql" /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */,
+  }
+);
 
-sequelize.authenticate()
+sequelize
+  .authenticate()
   .then(() => {
-    console.log('Connection has been established successfully.');
+    console.log("Connection has been established successfully.");
   })
-  .catch(err => {
-    console.error('Unable to connect to the database:', err);
+  .catch((err) => {
+    console.error("Unable to connect to the database:", err);
   });
 
 module.exports = sequelize;
