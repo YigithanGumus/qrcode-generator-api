b20860c020ed1bb6e65c8a4191223647630964da
YigithanGumus <yigithangumuss@gmail.com>
2024-11-19 01:06:03 +0300

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,23 +1,27 @@
-const { DataTypes } = require('sequelize');
-const sequelize = require('../config/database');
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/database");
 
-const User = sequelize.define('User', {
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false
+const User = sequelize.define(
+  "User",
+  {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false
+  {
+    tableName: "users",
+    timestamps: true,
   }
-}, {
-  tableName: 'users',
-  timestamps: true
-});
+);
 
 module.exports = User;
